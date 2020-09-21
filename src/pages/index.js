eaeeb686858e3c54bb5885efe311e77981f390ce
davidchang <changd8@uw.edu>
2020-09-21 14:42:22 -0700

    modified index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,16 @@
-import React from "react"
+import React from "react";
+import Header from "../components/header";
+import Layout from "../components/layout"
+
+export default function Home() {
+    return (
+        <Layout>
+            <Header headerText="About Me" />
+            <p>
+                I am an aspiring software developer and a rising junior at <br/> University of Washington, Seattle,
+                studying computer science.
+            </p>
+        </Layout>
+    );
+}
+
